d58ca7b8d809ce70facd847c1fb89ce15e5c8947
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-11-08 13:41:43 -0500

    allowed for entering a query question

--- api.py
+++ api.py
@@ -3,8 +3,7 @@ import os
 import random
 
 from aiohttp import web
-from aiohttp.web_request import Request
-from aiohttp.web_response import Response
+from aiohttp.web import Request, Response
 
 with open("./responses", "r") as resp:
     lines = [line.replace("\n", "") for line in resp.readlines()]
@@ -16,9 +15,10 @@ async def index(request: Request) -> Response:
 
 async def handle(request: Request) -> Response:
     ret = {
+        "question": request.query.get("question", "None"),
         "message": random.choice(lines),
     }
-    return web.Response(text=json.dumps(ret))
+    return web.json_response(ret)
 
 
 async def create_app() -> web.Application:
