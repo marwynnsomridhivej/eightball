dabe5f2e8ade84010a2c68b4ba894d05ea05ef49
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-11-08 13:20:03 -0500

    sadness

--- api.py
+++ api.py
@@ -5,15 +5,18 @@ import random
 from aiohttp import web
 from aiohttp.web_request import Request
 from aiohttp.web_response import Response
+from aiohttp.web_routedef import route
 
 with open("./api/responses", "r") as resp:
     lines = [line.replace("\n", "") for line in resp.readlines()]
 
 
+@route.get("/")
 async def index(request: Request) -> Response:
     return web.Response(text="Eightball response API - Marwynn Somridhivej")
 
 
+@route.get("/eightball")
 async def handle(request: Request) -> Response:
     ret = {
         "message": random.choice(lines),
@@ -23,11 +26,10 @@ async def handle(request: Request) -> Response:
 
 async def create_app() -> web.Application:
     app = web.Application()
-    app.router.add_get("/", index)
-    app.router.add_get("/eightball", handle)
+    app.add_routes(route)
     return app
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    web.run_app(create_app(), port=port)
+    web.run_app(create_app(), host="localhost", port=port)
